4b4fc4ba72df24da8147fef4df1410a16fec74f7
Donavan Jones <donavan@businessbenefitalliance.com>
2023-07-16 21:13:10 -0400

    first commit

---
+++ components/TwoColumnSection.vue
@@ -0,0 +1,83 @@
+<template>
+  <section class="container mx-auto mt-20">
+    <div class="grid grid-cols-2 gap-4 items-center" v-if="mediaFirst">
+      <div>
+        <p
+          class="mb-4 font-extrabold tracking-tight leading-none text-gray-900 dark:text-white"
+        >
+          {{ subTitle }}
+        </p>
+        <h1
+          class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+        >
+          {{ title }}
+        </h1>
+        <p
+          class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400"
+        >
+          {{ description }}
+        </p>
+        <div
+          class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-left sm:space-y-0 sm:space-x-4"
+        >
+          <button
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Learn More About Our Program
+          </button>
+        </div>
+      </div>
+      <div>
+        <img src="~/assets/images/contractor-marketing-program.jpg" />
+      </div>
+    </div>
+    <div class="grid grid-cols-2 gap-4 items-center" v-else>
+      <div>
+        <img src="~/assets/images/contractor-marketing-program.jpg" />
+      </div>
+      <div>
+        <p
+          class="mb-4 font-extrabold tracking-tight leading-none text-gray-900 dark:text-white"
+        >
+          {{ subTitle }}
+        </p>
+        <h1
+          class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+        >
+          {{ title }}
+        </h1>
+        <p
+          class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400"
+        >
+          {{ description }}
+        </p>
+        <div
+          class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-left sm:space-y-0 sm:space-x-4"
+        >
+          <button
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Learn More About Our Program
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TwoColumnSection",
+  props: {
+    title: String,
+    description: String,
+    mediaFirst: Boolean,
+    subTitle: String,
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,251 @@
+<template>
+  <div>
+    <HeroSection />
+    <TwoColumnSection
+      title="Your Services Are Being Searched For Online Daily By Potential Customers!"
+      subTitle="Empower your business to new heights with our marketing blueprint. Let us make it effortless for your customers to find you!"
+      description="At DJ Web Media, we create custom websites and marketing solutions for each client, taking the time to understand their business, products, competitors, and industry. Our data-driven strategies increase leads and sales while optimizing marketing costs, and we provide detailed analytics to track success. Let us help you differentiate your brand and succeed in the online marketplace."
+      :mediaFirst="true"
+    />
+    <TwoColumnSection
+      title="Engage With Your Ideal Customers"
+      subTitle="To Expand Your Business, Increase Online Leads, Calls, And Revenue"
+      description="To generate more leads and increase website traffic, it's crucial to improve your website's visibility in Google's organic and paid results. A digital marketing company can help ensure your website dominates the first page of search engine results. With 93% of online buyers starting their experience with a search, this is critical to maximizing profits."
+      :mediaFirst="false"
+    />
+    <TwoColumnSection
+      title="Transform Your Website into a Sales Generating Machine"
+      subTitle="Expert Website Design and Development"
+      description="Your website is a crucial component of your marketing strategy, and should be treated as a virtual sales representative, not just a brochure. If your website is outdated, fails to convert visitors into leads, or simply doesn't meet your expectations, it's time to enlist the services of a professional website design company. With our expertise, we can redesign and update your website to effectively convert web traffic into leads and boost your online presence."
+      :mediaFirst="true"
+    />
+
+    <section class="container mx-auto mt-20">
+      <p
+        class="mb-4 font-extrabold tracking-tight leading-none text-gray-900 dark:text-white"
+      >
+        Relying Solely On Referrals And Word Of Mouth Is No Longer Enough For
+        Businesses. Smart Business Owners Now Integrate Digital Marketing Into
+        Their Marketing Plan
+      </p>
+      <h2
+        class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+      >
+        Comprehensive Marketing Solutions
+      </h2>
+      <p
+        class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400"
+      >
+        Our team is dedicated to providing you with a comprehensive marketing
+        solution for your business. We start by building your website with no
+        extra build charges beyond our monthly fee. From there, our professional
+        marketing team can help you establish a complete marketing presence for
+        your business. Our services include paid online advertising with Google
+        and Facebook, social media management, email marketing, and printing
+        marketing services. Trust us to help you achieve your business goals
+        through a strong online and offline marketing strategy.
+      </p>
+    </section>
+
+    <section class="container mx-auto mt-20">
+      <div class="grid grid-cols-2 gap-4 items-center">
+        <CardDeckTwoColumns>
+          <ProductCard
+            title="Website Design"
+            image="~/assets/"
+            description="Drive Business Growth: Acquire New Clients, Lower Expenses, and Increase Profits!"
+            url=""
+          />
+          <ProductCard
+            title="Search Engine Optimization"
+            image=""
+            description="Incorporating Local SEO into Your Online Marketing Strategy"
+            url=""
+          />
+          <ProductCard
+            title="Lead Automation"
+            image=""
+            description="Eliminate Wasted Time by Focusing on High-Quality Leads"
+            url=""
+          />
+          <ProductCard
+            title="Ongoing Website Updates"
+            image=""
+            description="Keeping Your Website Up-to-Date with Regular Improvements"
+            url=""
+          />
+          <ProductCard
+            title="Website Hosting Services"
+            image=""
+            description="Secure Website and Email Hosting Services You Can Rely On"
+            url=""
+          />
+          <ProductCard
+            title="Developing a Comprehensive Marketing Strategy"
+            image=""
+            description="Tailor Your Marketing Strategy to Fit Your Business Needs"
+            url=""
+          />
+        </CardDeckTwoColumns>
+        <div>
+          <nuxt-img
+            format="webp"
+            src="~/assets/images/contractor-marketing-program.jpg"
+          />
+        </div>
+      </div>
+    </section>
+
+    <section class="mx-auto p-8 mt-20 bg-blue-400 text-center">
+      <p
+        class="mb-4 font-extrabold tracking-tight leading-none text-gray-900 text-white"
+      >
+        All From A Company That Exclusively Provides Marketing For Contractors
+      </p>
+      <h3
+        class="mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl text-white"
+      >
+        We Provide The Plan / To Get You The Leads /
+        <strong>That Keeps Your Schedule Full</strong>
+      </h3>
+    </section>
+
+    <TwoColumnSection
+      title="Establish Your Dominance In Your Niche With Our Web Marketing Solutions"
+      subTitle="Comprehensive Marketing Solutions To Elevate Your Business Beyond Just A Website"
+      description="Our team of marketing experts has extensive experience in designing and executing successful campaigns for businesses that aim to boost leads, phone calls, transactions, and web traffic. We will apply this expertise to your marketing strategy, too.
+While your website is an essential component of your online presence, it is just one of the tools in your marketing toolbox. It is not a comprehensive marketing plan in itself. Thats why our core program comprises all the vital components for maintaining your organic marketing presence. However, this is only the beginning.
+Our objective is to collaborate with you to develop a marketing plan that is customized to suit your companys specific requirements and advertising budget."
+      :mediaFirst="false"
+    />
+
+    <section class="container mx-auto mt-20">
+      <p
+        class="mb-4 font-extrabold tracking-tight leading-none text-gray-900 dark:text-white"
+      >
+        Examples of Recently Launched Websites
+      </p>
+      <h2
+        class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+      >
+        Launching A New Website Is Always An Exciting Event, And This Was
+        Undoubtedly The Case For These Recent Website Launches!
+      </h2>
+      <p
+        class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400"
+      >
+        Our team will construct your website without any additional build
+        charges beyond our monthly fee. Additionally, our skilled marketing team
+        can assist you in creating a comprehensive marketing strategy for your
+        business. This comprises paid online advertising via Google and
+        Facebook, social media management, email marketing, and printing
+        marketing services.
+      </p>
+    </section>
+
+    <CardDeckThreeColumns>
+      <LaunchCard
+        title="Epoxy Floor Solutions"
+        image=""
+        description="Acquire New Clients, Lower Expenses and Increase Profits!"
+        url=""
+      />
+      <LaunchCard
+        title="Innovative Garage Flooring"
+        image=""
+        description="Contractor SEO and Your Online Marketing Plan"
+        url=""
+      />
+      <LaunchCard
+        title="Lowcountry Powerwashing"
+        image=""
+        description="Satisifying Your Customers' Needs For Social Trust"
+        url=""
+      />
+    </CardDeckThreeColumns>
+
+    <section class="container mx-auto mt-20">
+      <p
+        class="mb-4 font-extrabold tracking-tight leading-none text-gray-900 dark:text-white"
+      >
+        The Industries We Serve
+      </p>
+      <h2
+        class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+      >
+        Promotion Is An Essential Component For Any Industry, And It's Crucial
+        To Understand That Each Industry Has Its Distinct Obstacles And Hurdles
+        To Overcome
+      </h2>
+      <p
+        class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400"
+      >
+        Each industry possesses distinctive marketing requirements that
+        necessitate the assistance of a strategic marketing partner to
+        accomplish their objectives and goals. At DJ Web Media, we grasp the
+        intricacies of these various industries and work closely with each
+        business to integrate ourselves into their team.
+      </p>
+    </section>
+
+    <CardDeckFourColumns>
+      <IndustryCard
+        title="Flooring"
+        image=""
+        description="Acquire New Clients, Lower Expenses and Increase Profits!"
+        url=""
+      />
+      <IndustryCard
+        title="Pressure Washing"
+        image=""
+        description="Contractor SEO and Your Online Marketing Plan"
+        url=""
+      />
+      <IndustryCard
+        title="Roofing"
+        image=""
+        description="Satisifying Your Customers' Needs For Social Trust"
+        url=""
+      />
+      <IndustryCard
+        title="Plumbing"
+        image=""
+        description="Satisifying Your Customers' Needs For Social Trust"
+        url=""
+      />
+      <IndustryCard
+        title="Flooring"
+        image=""
+        description="Acquire New Clients, Lower Expenses and Increase Profits!"
+        url=""
+      />
+      <IndustryCard
+        title="Pressure Washing"
+        image=""
+        description="Contractor SEO and Your Online Marketing Plan"
+        url=""
+      />
+      <IndustryCard
+        title="Roofing"
+        image=""
+        description="Satisifying Your Customers' Needs For Social Trust"
+        url=""
+      />
+      <IndustryCard
+        title="Plumbing"
+        image=""
+        description="Satisifying Your Customers' Needs For Social Trust"
+        url=""
+      />
+    </CardDeckFourColumns>
+
+    <Ribbon />
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+useHead({
+  title: "Home Page | Company",
+});
+</script>
